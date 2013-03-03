0c3e724614a082aaa464e93ab53295f9682c2da8
seishun <vvnicholas@gmail.com>
2013-03-03 17:27:43 +0000

    too many changes to fit in a single line
    - Switch to request because superagent sucks
    - Handle more TCP and HTTP errors
    - Emit 'complete' with an optional getItems function
    - Handle receiving currency (Crowns in Spiral Knights)
    - Wait for a response before polling again
    - Don't emit 'error' if a user action (like `addItem`) fails
    - Remove `cont()`, just let the user restart the trade
    - "Public" tradePartnerSteamID
    - Ignore trade responses when the trade is over
    - Replace `cookie` property with `setCookie` method
    - Add `getContexts` and `cancel` methods (closes #1)
    - Handle currency in `loadInventory`
    - Replace `addItem` with `addItems`, support stackable items
    - Don't attempt to re-confirm if the trade is over
    - Other things I forgot about

--- index.js
+++ index.js
@@ -1,185 +1,224 @@
 module.exports = SteamTrade;
 
+var request = require('request');
+var j = request.jar();
+request = request.defaults({jar:j});
+
 require('util').inherits(SteamTrade, require('events').EventEmitter);
 
 function SteamTrade() {
   require('events').EventEmitter.call(this);
 }
 
-SteamTrade.prototype._onLoadInventory = function(appid, contextid) {
-  return function(res) {
-    for (var id in res.body.rgInventory) {
-      var item = res.body.rgInventory[id];
-      var description = res.body.rgDescriptions[item.classid + '_' + item.instanceid];
+SteamTrade.prototype._loadForeignInventory = function(appid, contextid) {
+  var self = this;
+  
+  request.post({
+    uri: 'http://steamcommunity.com/trade/' + this.tradePartnerSteamID + '/foreigninventory',
+    headers: {
+      referer: 'http://steamcommunity.com/trade/1'
+    },
+    form: {
+      sessionid: this.sessionID,
+      steamid: this.tradePartnerSteamID,
+      appid: appid,
+      contextid: contextid
+    },
+    json: true
+  }, function(error, response, body) {
+    if (error) {
+      self.emit('debug', 'loading inventory: ' + error);
+      // retry
+      self._loadForeignInventory(appid, contextid);
+      return;
+    }
+    
+    for (var id in body.rgInventory) {
+      var item = body.rgInventory[id];
+      var description = body.rgDescriptions[item.classid + '_' + item.instanceid];
       for (var key in description) {
         item[key] = description[key];
       }
     }
     
-    if (!this._themInventories[appid]) {
-      this._themInventories[appid] = {};
+    if (!self._themInventories[appid]) {
+      self._themInventories[appid] = {};
     }
-    this._themInventories[appid][contextid] = res.body.rgInventory;
+    self._themInventories[appid][contextid] = body.rgInventory;
     
-    this._loadingInventoryData = false;
-  }.bind(this);
+    self._loadingInventoryData = false;
+  });
 };
 
-SteamTrade.prototype._onTradeStatusUpdate = function(callback) {
-  return function(res) {
-    clearTimeout(this._timerTradePoll);
+SteamTrade.prototype._onTradeStatusUpdate = function(body, callback) {  
+  clearTimeout(this._timerTradePoll);
+  
+  var self = this;
+  
+  if (body.success && body.trade_status !== 0) {
     
-    if (!res.body.success) {
-      this.emit('debug', 'trade fail');
-      callback(res.body);
-      return;
+    if (callback) {
+      // callback before emitting events
+      callback(body);
     }
     
-    if (res.body.trade_status !== 0) {
-      
-      if (res.body.trade_status == 1) {
-        require('superagent')
-          .get('http://steamcommunity.com/trade/' + res.body.tradeid + '/receipt/')
-          .set('Cookie', this.cookie)
-          .end(function(res) {
-            this.emit('end', 'complete', (res.text
-              .match(/oItem = [\s\S]+?amount = \d+;\r\n\toItem/g) || [])
-              .map(eval)
-            );
-          }.bind(this));
-      
-      } else {
-        this.emit('end', {
-          3: 'cancelled',
-          4: 'timeout',
-          5: 'failed'
-        }[res.body.trade_status]);
-      }
-      
-      return;
-    }
-    
-    this._timerTradePoll = setTimeout(function() {
-      this._send('tradestatus', {
-        logpos: this._nextLogPos,
-        version: this._version
-      });
-    }.bind(this), 1000);
-    
-    if (res.body.newversion)
-      // we can update our own assets safely
-      this._meAssets = res.body.me.assets;
-    
-    // callback now, otherwise we might return (loading inventory) and never get there
-    callback(res.body);
-    
-    if (this._loadingInventoryData) {
-      // we'll receive the same events again
-      return;
-    }
-    
-    var ready = false;
-    
-    // events might be undefined, but it's fine
-    for (var i in res.body.events) {
-      if (i < this._nextLogPos)
-        continue;
-      
-      var event = res.body.events[i];
-      
-      if (event.steamid != this._tradePartnerSteamID)
-        continue; // not interested in our own actions
-      
-      switch (event.action) {
-        case '0':
-        case '1':
-          var inventory = this._themInventories[event.appid] && this._themInventories[event.appid][event.contextid];
-          if (!inventory) {
-            require('superagent')
-              .post('http://steamcommunity.com/trade/' + this._tradePartnerSteamID + '/foreigninventory')
-              .set('Cookie', this.cookie)
-              .set('Referer', 'http://steamcommunity.com/trade/1')
-              .type('form')
-              .on('error', function(error) {
-                self.emit('debug', error);
-                // retry on next status update
-                this._loadingInventoryData = false;
-              })
-              .send({
-                sessionid: this.sessionID,
-                steamid: this._tradePartnerSteamID,
-                appid: event.appid,
-                contextid: event.contextid
-              })
-              .end(this._onLoadInventory(event.appid, event.contextid));
-            this._loadingInventoryData = true;
+    if (body.trade_status == 1) {
+      this.emit('end', 'complete', function getItems(callback) {
+        request.get('http://steamcommunity.com/trade/' + body.tradeid + '/receipt/', function(error, response, body) {
+          if (error || response.statusCode != 200) {
+            self.emit('debug', 'Opening receipt page: ' + (error || response.statusCode));
+            getItems(callback);
             return;
           }
-          this.emit('offerChanged', event.action == '0', inventory[event.assetid]);
-          break;
-        case '2':
-          // defer 'ready' event until we've handled anything else
-          ready = true;
-          break;
-        case '3':
-          ready = false;
-          this.emit('unready');
-          break;
-        case '7':
-          this.emit('chatMsg', event.text);
-      }
+          
+          var items = [];
+          
+          // prepare to execute the script in the page
+          var UserYou;
+          function BuildHover(str, item) {
+            items.push(item);
+          }
+          function $() {
+            return {
+              show: function() {}
+            };
+          }
+          
+          // evil magic happens here
+          eval(body.match(/(var oItem;[\s\S]*)<\/script>/)[1]);
+          
+          callback(items);
+        });
+      });
+    } else {
+      this.emit('end', {
+        3: 'cancelled',
+        4: 'timeout',
+        5: 'failed',
+        undefined: 'canceled'
+      }[body.trade_status]);
     }
     
-    if (i >= this._nextLogPos)
-      this._nextLogPos = ++i;
-    
-    if (res.body.newversion) {
-      // now that we know we have all inventories, we can update their assets too
-      this._themAssets = res.body.them.assets;
-      this._version = res.body.version;
-    }
-    
-    if (ready)
-      this.emit('ready');
+    delete this.tradePartnerSteamID;
+    return;
+  }
   
-  }.bind(this);
+  this._timerTradePoll = setTimeout(function() {
+    self._send('tradestatus', {
+      logpos: self._nextLogPos,
+      version: self._version
+    }, function(status) {
+      if (!status.success) {
+        // assume session lost, stop polling
+        self.emit('debug', JSON.stringify(status));
+        clearTimeout(self._timerTradePoll);
+        var err = new Error('Invalid cookie');
+        self.emit('error', err);
+      }
+    });
+  }, 1000);
+  
+  if (body.newversion)
+    // we can update our own assets safely
+    this._meAssets = body.me.assets;
+  
+  if (callback) {
+    // callback now, otherwise we might return (loading inventory) and never get there
+    callback(body);
+  }
+  
+  if (this._loadingInventoryData) {
+    // we'll receive the same events again
+    return;
+  }
+  
+  var ready = false;
+  
+  // events might be undefined, but it's fine
+  for (var i in body.events) {
+    if (i < this._nextLogPos)
+      continue;
+    
+    var event = body.events[i];
+    
+    if (event.steamid != this.tradePartnerSteamID)
+      continue; // not interested in our own actions
+    
+    switch (event.action) {
+      case '0':
+      case '1':
+        var inventory = this._themInventories[event.appid] && this._themInventories[event.appid][event.contextid];
+        if (!inventory) {
+          this._loadForeignInventory(event.appid, event.contextid);
+          this._loadingInventoryData = true;
+          return;
+        }
+        this.emit('offerChanged', event.action == '0', inventory[event.assetid]);
+        break;
+      case '2':
+        // defer 'ready' event until we've handled anything else
+        ready = true;
+        break;
+      case '3':
+        ready = false;
+        this.emit('unready');
+        break;
+      case '7':
+        this.emit('chatMsg', event.text);
+    }
+  }
+  
+  if (i >= this._nextLogPos)
+    this._nextLogPos = ++i;
+  
+  if (body.newversion) {
+    // now that we know we have all inventories, we can update their assets too
+    this._themAssets = body.them.assets;
+    this._version = body.version;
+  }
+  
+  if (ready)
+    this.emit('ready');
 };
 
 SteamTrade.prototype._send = function(action, data, callback) {
   clearTimeout(this._timerTradePoll);
   
+  data.sessionid = this.sessionID;
+  
   var self = this;
   
-  require('superagent')
-    .post('http://steamcommunity.com/trade/' + this._tradePartnerSteamID + '/' + action)
-    .set('Cookie', this.cookie)
-    .set('Referer', 'http://steamcommunity.com/trade/1')
-    .type('form')
-    .send({
-      sessionid: this.sessionID
-    })
-    .send(data)
-    .on('error', function(error) {
-      self.emit('debug', error);
+  request.post({
+    uri: 'http://steamcommunity.com/trade/' + this.tradePartnerSteamID + '/' + action,
+    headers: {
+      referer: 'http://steamcommunity.com/trade/1'
+    },
+    form: data,
+    json: true
+  }, function(error, response, body) {
+    if (!self.tradePartnerSteamID) {
+      // trade is over already
+      return;
+    }
+    
+    if (error || response.statusCode != 200) {
+      self.emit('debug', 'sending ' + action + ': ' + (error || response.statusCode));
+      // retry
       self._send(action, data, callback);
-    })
-    .end(this._onTradeStatusUpdate(function(res) {
-      if (!res.success) {
-        var err = new Error('Invalid cookie');
-        err.cont = function() {
-          self._send(action, data, callback);
-        }
-        self.emit('error', err);
-        
-      } else if (callback) {
-        callback(res);
-      }
-    }));  
+      return;
+    }
+    
+    self._onTradeStatusUpdate(body, callback);
+    return;
+  });
 };
 
+SteamTrade.prototype.setCookie = function(cookie) {
+  j.add(request.cookie(cookie));
+};
 
 SteamTrade.prototype.open = function(steamID, callback) {
-  this._tradePartnerSteamID = steamID;
+  this.tradePartnerSteamID = steamID;
   this._themInventories = {};
   this._themAssets = [];
   this._meAssets = [];
@@ -192,27 +231,39 @@ SteamTrade.prototype.open = function(steamID, callback) {
   }, callback);
 };
 
-SteamTrade.prototype.loadInventory = function(appid, contextid, callback) {
-  require('superagent')
-    .get('http://steamcommunity.com/my/inventory/json/' + appid + '/' + contextid)
-    .set('Cookie', this.cookie)
-    .on('error', function(error) {
-      self.emit('debug', error);
-      self.loadInventory(appid, contextid, callback);
-    })
-    .end(function(res) {
-      callback(Object.keys(res.body.rgInventory).map(function(id) {
-        var item = res.body.rgInventory[id];
-        var description = res.body.rgDescriptions[item.classid + '_' + item.instanceid];
-        for (var key in description) {
-          item[key] = description[key];
-        }
-        // add contextid because Steam is retarded
-        item.contextid = contextid;
-        return item;
-      }));
-    });
+SteamTrade.prototype.getContexts = function(callback) {
+  request.get('http://steamcommunity.com/trade/' + this.tradePartnerSteamID, function(error, response, body) {
+    callback(JSON.parse(body.match(/var g_rgAppContextData = (.*);/)[1]));
+  });
 };
+
+SteamTrade.prototype.loadInventory = function(appid, contextid, callback) {
+  request.get({
+    uri: 'http://steamcommunity.com/my/inventory/json/' + appid + '/' + contextid,
+    json: true
+  }, function(error, response, body) {
+    if (error) {
+      this.emit('debug', 'loading my inventory: ' + error);
+      this.loadInventory(appid, contextid, callback);
+      return;
+    }
+    callback(mergeWithDescriptions(body.rgInventory, body.rgDescriptions, contextid)
+      .concat(mergeWithDescriptions(body.rgCurrency, body.rgDescriptions, contextid)));
+  }.bind(this));
+};
+
+function mergeWithDescriptions(items, descriptions, contextid) {
+  return Object.keys(items).map(function(id) {
+    var item = items[id];
+    var description = descriptions[item.classid + '_' + (item.instanceid || '0')];
+    for (var key in description) {
+      item[key] = description[key];
+    }
+    // add contextid because Steam is retarded
+    item.contextid = contextid;
+    return item;
+  });
+}
 
 SteamTrade.prototype.themAssets = function() {
   return this._themAssets.map(function(item) {
@@ -220,21 +271,31 @@ SteamTrade.prototype.themAssets = function() {
   }.bind(this));
 };
 
-SteamTrade.prototype.addItem = function(item, callback, slot) {
-  // find first free slot
-  if (slot === undefined) {
-    if (this._meAssets === '') // it can be either an array, a string or an object -_-
-      slot = 0;
-    else
-      for (slot = 0; slot in this._meAssets; slot++);
-  }
+SteamTrade.prototype.addItems = function(items, callback) {
+  var count = items.length;
+  var slot = 0;
+  var results = [];
   
-  this._send('additem', {
-    appid: item.appid,
-    contextid: item.contextid,
-    itemid: item.id,
-    slot: slot
-  }, callback);
+  items.forEach(function(item, index) {
+    // find first free slot
+    for (; this._meAssets && slot in this._meAssets; slot++);
+    
+    this._send(item.is_currency ? 'setcurrency' : 'additem', {
+      appid: item.appid,
+      contextid: item.contextid,
+      
+      itemid: item.id,
+      currencyid: item.id,
+      
+      slot: slot++, // it's taken tentatively
+      amount: item.amount
+    }, function(res) {
+      results[index] = res;
+      if (!--count) {
+        callback(results);
+      }
+    });
+  }.bind(this));
 };
 
 SteamTrade.prototype.removeItem = function(item, callback) {
@@ -268,9 +329,18 @@ SteamTrade.prototype.confirm = function(callback) {
     // so we'll have to resend the confirm if this one failed
     // but only if it _should_ have worked
     
+    if (status.trade_status !== 0) {
+      // trade is over
+      return;
+    }
+    
     if (!status.me.confirmed && status.me.ready && status.them.ready) {
-      this.emit('debug', 'Steam dumbed');
+//      this.emit('debug', 'Steam dumbed');
       this.confirm(callback);
     }
   }.bind(this));
+};
+
+SteamTrade.prototype.cancel = function(callback) {
+  this._send('cancel', {}, callback);
 };
