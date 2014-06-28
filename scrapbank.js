39544a7d6db89aa9baf9ad725c9ac58e4fa0cb75
Alexey Komarov <alex7kom@gmail.com>
2014-06-28 14:23:27 +0600

    Update scrapbank.js example to work with newer versions of node-steam

--- scrapbank.js
+++ scrapbank.js
@@ -1,9 +1,17 @@
-var username = '';
-var password = '';
-var steamGuard = require('fs').existsSync('sentry') ? require('fs').readFileSync('sentry')
-    : ''; // code received by email
 var admin = ''; // put your steamid here to say 'give' to the bot and receive all non-scrap items
 
+var logOnOptions = {
+  accountName: '',
+  password: ''
+};
+
+var authCode = ''; // code received by email
+
+if (require('fs').existsSync('sentry')) {
+  logOnOptions['shaSentryfile'] = require('fs').readFileSync('sentry');
+} else if (authCode != '') {
+  logOnOptions['authCode'] = authCode;
+}
 
 var Steam = require('steam');
 var SteamTrade = require('./'); // change to 'steam-trade' if not running from the same directory
@@ -11,7 +19,7 @@ var SteamTrade = require('./'); // change to 'steam-trade' if not running from t
 var steam = new Steam.SteamClient();
 var steamTrade = new SteamTrade();
 
-steam.logOn(username, password, steamGuard);
+steam.logOn(logOnOptions);
 
 steam.on('debug', console.log);
 
@@ -25,7 +33,7 @@ steam.on('webSessionID', function(sessionID) {
   steamTrade.sessionID = sessionID;
   steam.webLogOn(function(cookies) {
     console.log('got a new cookie:', cookies);
-    cookies.split(';').forEach(function(cookie) {
+    cookies.forEach(function(cookie) {
         steamTrade.setCookie(cookie);
     });
   });
